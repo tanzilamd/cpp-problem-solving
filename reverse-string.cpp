71b425cba4f757f6e98fce1fa465ae5b840ecf6e
tanzilamd <mdtanzil858@gmail.com>
2025-11-01 23:04:24 +0600

    reverse string

---
+++ reverse-string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    string s = "Hello World!";
+
+    reverse(s.begin(), s.end());
+
+    cout<<s;
+}
+
+int main(){
+    int t;cin>>t;
+    while(t--) solution();
+
+    return 0;
+}
---
+++ reverse-string2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    string s = "Hello";
+
+    int l = s.size() - 1;
+    int i = 0;
+
+    while(i < l) {
+        swap(s[i], s[l]);
+
+        l--;
+    }
+
+    cout<<s<<endl;
+}
+
+int main(){
+    int t;cin>>t;
+    while(t--) solution();
+
+    return 0;
+}
